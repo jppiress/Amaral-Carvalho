06fc443dca9cefd75b3c33871375e81eedf241da
GuilhermeARN <guilhermearn@hotmail.com>
2026-05-24 23:47:38 -0300

    Add files via upload

--- Tela de login Amaral/script.js
+++ Tela de login Amaral/script.js
@@ -5,9 +5,9 @@ const inputs = document.querySelectorAll( "input" );
 const loginButton = document.getElementById( "login.button ");
 
 function switchTheme() {
-    loginBox.classList.toggle( "dark-mode ");
-    body.classList.toggle( "dark-mode ");
-    h1.classList.toggle( "dark-mode ");
+    loginBox.classList.toggle( "dark-mode");
+    body.classList.toggle( "dark-mode");
+    h1.classList.toggle( "dark-mode");
     inputs.forEach( input => {
         input.classList.toggle( "dark-mode" );
     } );
